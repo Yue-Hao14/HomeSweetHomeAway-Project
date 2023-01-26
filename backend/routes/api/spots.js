f01734cda492138a5baf10fd077df44f3d3bc7f7
Yue-Hao14 <hyailsa0114@gmail.com>
2023-01-26 16:22:31 -0500

    get all spots owned by the current user endpoint done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,7 +4,65 @@ const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const router = express.Router();
 
 // Get spots of current user
-router.get('/', restoreUser, async (req, res, next) => {
+router.get('/current', restoreUser, async (req, res, next) => {
+  let result = {};
+  
+  // extract user object (promise) from restoreUser middleware output
+  const { user } = req;
+
+  // convert user to normal POJO
+  const userPOJO = user.toJSON();
+  // console.log(userPOJO)
+
+  // get userId of the current user
+  const userId = userPOJO.id
+  // console.log(userId);
+
+  const spots = await Spot.findAll({
+    include: [Review, SpotImage],
+    where: {
+      ownerId: userId
+    }
+  })
+
+  // initiate spotsArr to host formatted spots
+  let spotsArr = [];
+
+  // iterate through each spot object
+  spots.forEach(spot => {
+    let totalRating = 0;
+    let totalCount = 0;
+
+    // convert each spot to a normal POJO
+    spot = spot.toJSON();
+
+    // iterate through Review to calculate avgRating
+    spot.Reviews.forEach(review => {
+      totalCount++;
+      totalRating += review.stars
+    });
+    let avgRating = totalRating / totalCount;
+    spot.avgRating = avgRating;
+
+    // iterate through SpotImages to set url to previewImage
+    spot.SpotImages.forEach(spotImage => {
+      if (spotImage.preview === true) {
+        let previewImage = spotImage.url
+        spot.previewImage = previewImage
+      }
+    })
+
+    // remove Review and SpotImage objects
+    delete spot.Reviews;
+    delete spot.SpotImages;
+
+    // push each formatted spot to spotsArr
+    spotsArr.push(spot);
+  });
+
+  result.Spots = spotsArr;
+
+  return res.json(result)
 
 })
 
