ce69414d3c24570a69d238844ef884783946ae2d
Yue-Hao14 <hyailsa0114@gmail.com>
2023-01-28 18:41:51 -0500

    Add an Image to a Review based on the Review's id

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -76,4 +76,82 @@ router.get('/current', restoreUser, async (req, res, _next) => {
 })
 
 
+
+// ------------------------------------------------------
+// Add an Image to a Review based on the Review's id
+router.post('/:reviewId/images', restoreUser, async (req, res, _next) => {
+  const reviewId = req.params.reviewId
+
+  // extract logged in/current user object (promise) from restoreUser middleware output
+  const { user } = req;
+  // convert user object to normal POJO
+  const userPOJO = user.toJSON();
+  // console.log(userPOJO)
+  // get userId of the current user
+  const userId = userPOJO.id
+  // console.log(userId);
+
+  // check if a review can be found, if not, error
+  const review = await Review.findOne({
+    where: {
+      id: reviewId
+    }
+  })
+  if (!review) {
+    return res.status(404).json({
+      message: "Review couldn't be found",
+      statusCode: 404
+    })
+  }
+
+
+  // check if review belongs to current user, if not, error
+  const reviewOfUser = await Review.findOne({
+    where: {
+      userId: userId,
+      id: reviewId
+    }
+  })
+  if (!reviewOfUser) {
+    return res.status(404).json({
+      message: "You don't have permission to add an image for this review",
+      statusCode: 404
+    })
+  }
+
+
+  // check # of reviewImages associated to this review, if already 10, error
+  const reviewImages = await ReviewImage.findAll({
+    where: {
+      reviewId: reviewId
+    }
+  })
+  if (reviewImages.length === 10) {
+    return res.status(403).json({
+      message: "Maximum number of images for this resource was reached",
+      statusCode: 403
+    })
+  }
+
+
+  // add this reviewImage
+  const { url } = req.body;
+  const image = await ReviewImage.create({
+    reviewId,
+    url
+  })
+
+  const newImage = await ReviewImage.findOne({
+    where: {
+      url: url
+    },
+    attributes: {
+      exclude: ['reviewId','createdAt','updatedAt']
+    }
+  })
+
+  return res.json(newImage);
+})
+
+
 module.exports = router;
